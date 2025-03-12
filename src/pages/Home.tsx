124d0f0c18cb2d555127c22b00007c2f8144c979
ducvietdev <ducthichcode@gmail.com>
2025-03-12 16:26:09 +0700

    update code

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,20 +3,27 @@ import { useNavigate } from "react-router-dom";
 const Home = () => {
   const navigate = useNavigate();
   return (
-    <div className="container text-center mt-5" style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+    <div
+      className="container text-center mt-5"
+      style={{ display: "flex", flexDirection: "column", alignItems: "center" }}
+    >
       <h1>PHẦN MỀM THI TRẮC NGHIỆM DÂN VẬN</h1>
-      <button
-        className="btn btn-primary mt-3"
-        onClick={() => navigate("/select")}
-      >
-        Bắt đầu
-      </button>
-      <button
-        className="btn btn-primary mt-3"
-        onClick={() => navigate("/exam")}
-      >
-        Thi thử
-      </button>
+      <div style={{ display: "flex", flexDirection: "row", alignItems: "center", gap: "1rem" }}>
+        <button
+          style={{ border: 'solid 1px green', color: "green" }}
+          className="btn btn-primary mt-3"
+          onClick={() => navigate("/select")}
+        >
+          Luyện tập
+        </button>
+        <button
+          style={{ border: 'solid 1px red', color: "red" }}
+          className="btn btn-primary mt-3"
+          onClick={() => navigate("/exam")}
+        >
+          Thi thử
+        </button>
+      </div>
     </div>
   );
 };
